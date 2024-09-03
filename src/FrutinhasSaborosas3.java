b5f47fad3c920cd0b411541bbdd00baf362d8d2e
TreinLuis <luis.trein@edu.pucrs.br>
2024-09-03 15:32:25 -0300

    Processando a arvore de baixo para cima no FRUTINHAS 3

--- src/FrutinhasSaborosas3.java
+++ src/FrutinhasSaborosas3.java
@@ -7,10 +7,15 @@ import java.util.*;
 public class FrutinhasSaborosas3 {
 
     static class Node {
-        int value;
+        String value;
+
         List<Node> children;
 
-        Node(int value) {
+//        Node(int value) {
+//            this.value = value;
+//            this.children = new ArrayList<>();
+//        }
+        Node(String value) {
             this.value = value;
             this.children = new ArrayList<>();
         }
@@ -24,87 +29,103 @@ public class FrutinhasSaborosas3 {
             String line;
             while ((line = br.readLine()) != null) {
                 lines.add(line);
-                System.out.println(line);
+               //System.out.println(line);
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Arquivo não encontrado: " + fileName, e);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        char[][] arvoreLida = convertLinesToMatrix(lines);
+        processarArvore(arvoreLida);
 
-        char[][] treeMatrix = convertLinesToMatrix(lines);
-
-        System.out.println("Estrutura da árvore:");
-
-        System.out.println("\nCaminhos e somas:");
+//        System.out.println("Estrutura da árvore:");
+//
+//        System.out.println("\nCaminhos e somas:");
+        //System.out.println(treeMatrix);
     }
 
     private static char[][] convertLinesToMatrix(List<String> lines) {
-        int numRows = lines.size();
-        int numCols = lines.stream().mapToInt(String::length).max().orElse(0);
+        int numeroLinhas = lines.size();
+        int numeroColunas = lines.stream().mapToInt(String::length).max().orElse(0);
 
-        char[][] treeMatrix = new char[numRows][numCols];
+        char[][] treeMatrix = new char[numeroLinhas][numeroColunas];
 
-        for (int i = 0; i < numRows; i++) {
+        for (int i = 0; i < numeroLinhas; i++) {
             String line = lines.get(i);
             for (int j = 0; j < line.length(); j++) {
                 treeMatrix[i][j] = line.charAt(j);
-                System.out.println(treeMatrix[i][j]);
+                //System.out.println(treeMatrix[i][j]);
             }
         }
-
         System.out.println("Matriz da árvore:");
         for (char[] row : treeMatrix) {
             System.out.println(Arrays.toString(row));
         }
-
         return treeMatrix;
     }
 
-    private static Node processarArvore(char[][] treeMatrix) {
-        int numRows = treeMatrix.length;
-        int numCols = treeMatrix[0].length;
+//    private static Node processarArvore(char[][] treeMatrix) {
+//        int numRows = treeMatrix.length;
+//        int numCols = treeMatrix[0].length;
+//        Node[][] nodes = new Node[numRows][numCols];
+//
+//        for (int i = 0; i < numRows; i++) {
+//            for (int j = 0; j < numCols; j++) {
+//                char current = treeMatrix[i][j];
+//                if (Character.isDigit(current)) {
+//                    nodes[i][j] = new Node(Character.getNumericValue(current));
+//                }
+//            }
+//        }
+//        for (int i = numRows - 1; i >= 0; i--) {
+//            for (int j = 0; j < numCols; j++) {
+//                Node currentNode = nodes[i][j];
+//                if (currentNode != null) {
+//                    if (i + 1 < numRows) {
+//                        if (j < numCols && nodes[i + 1][j] != null) {
+//                            currentNode.children.add(nodes[i + 1][j]);
+//                        }
+//                        if (j + 1 < numCols && treeMatrix[i + 1][j + 1] == '/') {
+//                            if (nodes[i + 1][j + 1] != null) {
+//                                currentNode.children.add(nodes[i + 1][j + 1]);
+//                            }
+//                        }
+//                        if (j - 1 >= 0 && treeMatrix[i + 1][j - 1] == '\\') {
+//                            if (nodes[i + 1][j - 1] != null) {
+//                                currentNode.children.add(nodes[i + 1][j - 1]);
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        Node root = null;
+//        for (int j = 0; j < numCols; j++) {
+//            if (nodes[0][j] != null) {
+//                root = nodes[0][j];
+//                break;
+//            }
+//        }
+//        return root;
+//    }
+
+    public static void processarArvore(char[][] arvore){
+        int numRows = arvore.length;
+        int numCols = arvore[0].length;
         Node[][] nodes = new Node[numRows][numCols];
+        System.out.println(numRows);
 
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                char current = treeMatrix[i][j];
-                if (Character.isDigit(current)) {
-                    nodes[i][j] = new Node(Character.getNumericValue(current));
-                }
+        for(int i=numRows-1; i > 0 ;i--){
+            //este for começa a processar a arvore de baixo para cima
+            for(int j = 0; j < numCols; j++){//aqui fica normal pois é da esquerda pra direita
+                String valor = String.valueOf(arvore[i][j]);
+                    nodes[i][j] = new Node(valor);
+                    System.out.println(nodes[i][j].value);
+
             }
         }
 
-        for (int i = numRows - 1; i >= 0; i--) {
-            for (int j = 0; j < numCols; j++) {
-                Node currentNode = nodes[i][j];
-                if (currentNode != null) {
-                    if (i + 1 < numRows) {
-                        if (j < numCols && nodes[i + 1][j] != null) {
-                            currentNode.children.add(nodes[i + 1][j]);
-                        }
-                        if (j + 1 < numCols && treeMatrix[i + 1][j + 1] == '/') {
-                            if (nodes[i + 1][j + 1] != null) {
-                                currentNode.children.add(nodes[i + 1][j + 1]);
-                            }
-                        }
-                        if (j - 1 >= 0 && treeMatrix[i + 1][j - 1] == '\\') {
-                            if (nodes[i + 1][j - 1] != null) {
-                                currentNode.children.add(nodes[i + 1][j - 1]);
-                            }
-                        }
-                    }
-                }
-            }
-        }
 
-        Node root = null;
-        for (int j = 0; j < numCols; j++) {
-            if (nodes[0][j] != null) {
-                root = nodes[0][j];
-                break;
-            }
-        }
-        return root;
     }
 }
